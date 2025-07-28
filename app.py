1718a796d72a44fde343525926db0a9ebe6d2c92
Your Name <you@example.com>
2025-07-28 11:17:45 -0500

    testing azure deployment1

--- app.py
+++ app.py
@@ -1418,4 +1418,4 @@ import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Get port from env or default to 5000
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port) # Allows for Azure port
